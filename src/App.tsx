a22f170f2565477138e78b4e043a0b9df28e139f
maximLomako <maxim.lomako@icloud.com>
2020-09-30 15:28:57 +0300

    feat: add todolist component, add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import './App.css';
+import {TaskType, Todolist} from "./Todolist";
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+
+  let task1: Array<TaskType> = [
+    {id: 1, title: 'css', isDone: true},
+    {id: 2, title: 'html', isDone: true},
+    {id: 3, title: 'react', isDone: false}
+  ]
+
+  let task2: Array<TaskType> = [
+    {id: 1, title: 'terminator', isDone: false},
+    {id: 2, title: 'xxx', isDone: true},
+    {id: 3, title: 'harry potter', isDone: true}
+  ]
+
+
+  return (
+    <div className='App'>
+      <Todolist title='What to learn' tasks={task1}/>
+      <Todolist title='Movies' tasks={task2}/>
+    </div>
+  );
 }
 
 export default App;
