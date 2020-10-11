1ff919e100c25adb94e10450d77c7a59e3d94201
maximLomako <maxim.lomako@icloud.com>
2020-10-11 15:54:02 +0300

    feat: add addTask func, refactor: func in todolist compomnent

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from "./Todolist";
+import { v1 } from 'uuid';
 
 
 export type FilterValuesType = 'all' | 'completed' | 'active';
 
 function App() {
   let [tasks, setTasks] = useState<Array<TaskType>>([
-    {id: 1, title: 'css', isDone: true},
-    {id: 2, title: 'html', isDone: true},
-    {id: 3, title: 'react', isDone: false}
+    {id: v1(), title: 'css', isDone: true},
+    {id: v1(), title: 'html', isDone: true},
+    {id: v1(), title: 'react', isDone: false}
   ]);
   let [filter, setFilter] = useState<FilterValuesType>('all');
 
 
-  function removeTask(id: number) {
+
+  function addTask(title: string) {
+    let newTask = {id: v1(), title: title, isDone: false};
+    let newTasks = [newTask, ...tasks];
+    setTasks(newTasks);
+  }
+
+
+  function removeTask(id: string) {
     let filteredTasks = tasks.filter(t => t.id !== id)
     setTasks(filteredTasks);
   }
@@ -38,6 +47,7 @@ function App() {
     <div className='App'>
       <Todolist title='What to learn'
                 tasks={tasksForTodolist}
+                addTask={addTask}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
 
